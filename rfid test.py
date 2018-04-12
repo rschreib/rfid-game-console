4d0853df7adb285686703f7e2a19fa2f99bac82d
rschreib <rschreib@mymail.mines.edu>
2018-04-12 11:48:17 -0600

    auto commit

--- rfid test.py
+++ rfid test.py
@@ -8,6 +8,7 @@ import time
 from time import sleep
 import RPi.GPIO as GPIO
 import math
+import serial
 
 pin20 = 20
 GPIO.setmode(GPIO.BCM)
@@ -17,17 +18,37 @@ GPIO.setup(pin20,GPIO.IN)
 # bus = smbus.SMBus(1)
 flag = 0
 firstvalue = 9
+ser = serial.Serial(port=pin20,baudrate=9600,parity=serial.PARITY_NONE)
 try:
     while True:
-        value = GPIO.input(pin20)
+        value = ser.readline()
+        # value = GPIO.input(pin20)
         print(value)
         if flag == 0:
             flag = 1
             firstvalue = GPIO.input(pin20)
         if value != firstvalue:
             break
+'''
+#!/usr/bin/env python
+import time
+import sys
 
+card = '0019171125'        # Stored good card number consider using a list or a file.
 
+def main():                # define a main function.
+    while True:            # loop until the program encounters an error.
+        sys.stdin = open('/dev/tty0', 'r')
+        RFID_input = input()
+        if RFID_input == card:      # compare the stored number to the input and if True execute code.
+            print "Access Granted"
+            print "Read code from RFID reader:{0}".format(RFID_input)
+        else:                    # and if the condition is false excecute this code.
+            print "Access Denied"
+
+# where is tty defined??
+            tty.close()
+'''
 #capture the control c and exit cleanly
 except(KeyboardInterrupt, SystemExit):
     print("User requested exit... bye!")
