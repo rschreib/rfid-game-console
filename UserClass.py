c05080f7ee3ae45c548adb46b637fe9f349f687d
rschreib <rschreib@mymail.mines.edu>
2018-04-12 15:38:45 -0600

    auto commit

---
+++ UserClass.py
@@ -0,0 +1,18 @@
+import datetime
+
+class User():
+   def __init__(self, name, age):
+       self.name = name
+       self.age = age
+       self.dateJoined = datetime.now()
+       self.wins = 0
+       self.losses = 0
+   def __repr__(self):
+       print("Name:\t{}".format(self.name))
+       print("age:\t{}".format(self.age))
+       print("Date Joined:\t{}".format(self.dateJoined))
+       print("Wins: {} Losses: {}".format(self.wins,self.losses))
+   def increment_wins(self):
+       self.wins += 1
+   def increment_losses(self):
+       self.losses += 1
