2f0ad3fc73b121387dc7f3805b2afebbbb49a51e
rschreib <rschreib@mymail.mines.edu>
2018-04-12 16:30:30 -0600

    auto commit

--- PracticeGame.py
+++ PracticeGame.py
@@ -1,28 +1,24 @@
 
 from time import sleep
 
-input = input("Play a game? [Y/n] ")
-acceptedResponses = ['y','Y','yes','Yes','YES','']
-playGame = False
-
-for response in acceptedResponses:
-    if (input == response):
-        playGame = True
-        print("playing game")
-
-if (not(playGame)):
-    print("Okay, Cya Later!")
-    exit()
+def StartGame():
+    input = input("Play a game? [Y/n] ")
+    acceptedResponses = ['y','Y','yes','Yes','YES','']
+    playGame = False
+    gameStartCounter = 3
 
 
-print("Game starting in...")
-count = 3
-for i in range(count):
-    print("{}".format(count-i))
-    sleep(.5)
+    for response in acceptedResponses:
+        if (input == response):
+            playGame = True
+            print("playing game")
 
-player1 = raw_input("Username: ")
+    if (not(playGame)):
+        print("Okay, Cya Later!")
+        exit()
 
 
-def startGame():
-    loadPlayerData
+    print("Game starting in...")
+    for i in range(gameStartCounter):
+        print("{}".format(gameStartCounter-i))
+        sleep(.5)
