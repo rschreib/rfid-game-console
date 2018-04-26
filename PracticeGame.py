511e4ad14fa7e195b392fb39c825cf726ff4cba4
rschreib <rschreib@mymail.mines.edu>
2018-04-26 13:39:09 -0600

    auto commit

--- PracticeGame.py
+++ PracticeGame.py
@@ -1,6 +1,8 @@
 from time import sleep
 from os import system
 import numpy
+import tictactoe
+import fourinarow
 
 def StartGame():
     response = input("Play a game? [Y/n] ")
@@ -15,12 +17,25 @@ def StartGame():
 
     if (not(playGame)):
         print("Okay, Cya Later!")
+        exit()
+
+    response = 1
+    print("1: Tic Tac Toe\n")
+    print("2: Four in a Row\n")
+    response = input("Select a game:")
 
     print("Game starting in...")
     for i in range(gameStartCounter):
         print("{}".format(gameStartCounter-i))
         sleep(.5)
 
+    if (response == '1'):
+        tictactoe.main()
+        input("\n\nPress the enter key to quit.")
+    elif (response == '2'):
+        fourinarow.main()
+    else:
+        exit()
 
 def does_user_exist(userID):
     for user in open("usernames.txt").readlines():
@@ -29,4 +44,6 @@ def does_user_exist(userID):
             return(True)
     return(False)
 
-print(does_user_exist("Robert"))
+# print(does_user_exist("Robert"))
+
+StartGame()
